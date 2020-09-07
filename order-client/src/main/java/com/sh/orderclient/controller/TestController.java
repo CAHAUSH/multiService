033e2fc8672c460fbc86a61c7e665a00194f3719
sunhang <1394829408@qq.com>
2020-09-07 16:11:41 +0800

    add post controller

--- order-client/src/main/java/com/sh/orderclient/controller/TestController.java
+++ order-client/src/main/java/com/sh/orderclient/controller/TestController.java
@@ -1,16 +1,20 @@
 package com.sh.orderclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
     @Value("${server.port}")
     private String port;
 
-    @RequestMapping("/hello")
-    public String heloo(){
+    @PostMapping("/hello")
+    public String heloo(@RequestBody String jsonStr){
+        return "this is a beautiful world from port:"+port+",name is "+jsonStr;
+    }
+
+    @RequestMapping("/test")
+    public String test(){
         return "this is a beautiful world from port:"+port;
     }
 }
